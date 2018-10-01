d037f3d6219695fd0296033dfe6597f7ff80a096
mauriciohidalgo <mauriciohidalgo@192.168.0.6>
2018-10-01 10:54:50 -0600

    Info Completed

--- ConnectFourService/src/info/index.php
+++ ConnectFourService/src/info/index.php
@@ -1,1 +1,5 @@
 <?php
+$strategies = array("Smart" => "SmartStrategy", "Random" => "RandomStrategy");
+$info = array("width" => 7, "height" => 6, "strategies" => array_keys($strategies));
+echo json_encode($info); 
+?>
